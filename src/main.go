67af0751f6a8459689af55b3a7cbbd9eae8cefe1
Turt-le-go <turtlego@PC.localdomain>
2022-01-25 05:39:47 +0200

    init

---
+++ src/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"os"
+	"fmt"
+	"time"
+	"bufio"
+	"strings"
+
+	"todo_client/src/utils"
+	"todo_client/src/todo"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const(
+	connPort int = 8080
+	connHost string = "localhost"
+
+	timeLayout string = "02.01 15:04"
+)
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main(){
+	fmt.Println("Client started")
+
+	conn, err := grpc.Dial(fmt.Sprintf("%v:%d", connHost, connPort), grpc.WithInsecure())
+	utils.Check(err)
+	defer conn.Close()
+
+	c := todo.NewToDoServiceClient(conn)
+	
+	commandLoop(c)
+}
+
+func commandLoop(c todo.ToDoServiceClient){
+	for {
+		input := getInput("-> ")
+		switch input {
+		case "add":
+			addTask(c)
+		case "list":
+			printAllTasks(c)
+		case "q":
+			fmt.Println("Bye.")
+			return
+		}
+	}
+}
+
+func getInput(prompt string) string {
+		fmt.Print(prompt)
+		input,_ := reader.ReadString('\n')
+		return strings.Trim(input," \n")
+}
+
+func addTask(c todo.ToDoServiceClient){
+	title := getInput("Task title: ")
+	timeString := getInput("Deadline: ")
+	deadline, err := parseTime(timeString)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Printf("Can't convert %v to time.\n", timeString)
+		fmt.Printf("Use this %v format.\n", timeLayout)
+		timeString = getInput("Deadline: ")
+		deadline, err = parseTime(timeString)
+		if err != nil {
+			fmt.Printf("Can't convert %v to time.\n", timeString)
+			fmt.Println("Failed to create task")
+			return
+		}
+	}
+	task := todo.TaskMessage{
+		Title: title,
+		Deadline: deadline,
+	}
+	res, err := c.AddTask(context.Background(), &task)
+	utils.Check(err)
+	fmt.Println(res.Text)
+}
+
+func parseTime(timeString string) (int32, error) {
+	t, err := time.Parse(timeLayout, timeString)
+	t = t.In(time.Now().Location())
+	return int32(t.Unix()), err
+}
+
+func printAllTasks(c todo.ToDoServiceClient) {
+	tasksList, err := c.ListTasks(context.Background(), &todo.Empty{})
+	utils.Check(err)
+	for _,task := range tasksList.List {
+		fmt.Printf(
+			"Title: %v, deadline: %v\n",
+			task.Title,
+			time.Unix(int64(task.Deadline), 0).Format(timeLayout),
+		)
+	}
+}
